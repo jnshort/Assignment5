e6e0ce750147c6ce6ad4b288dada2d8b68e5ed87
jnshort <justinnshort98@gmail.com>
2024-10-20 00:28:14 -0700

    account for other errors

--- server.py
+++ server.py
@@ -3,16 +3,19 @@ import socket
 
 def main():
     myTCPSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    myTCPSocket.bind(('localhost', 1234))
+    myTCPSocket.bind(('192.168.1.151', 1234))
     myTCPSocket.listen(5)
 
     incomingSocket, incomingAddress = myTCPSocket.accept()
     while True:
-        mesrecv = str(incomingSocket.recv(1024))
-        mesrecv = mesrecv.upper()
+        try:
+            mesrecv = str(incomingSocket.recv(1024))
+            mesrecv = mesrecv.upper()
+        except (ConnectionAbortedError):
+            break
         try:
             incomingSocket.send(bytearray(mesrecv, encoding='utf-8'))
-        except (BrokenPipeError, ConnectionResetError):
+        except (BrokenPipeError, ConnectionResetError, ConnectionAbortedError):
             break
     incomingSocket.close()
     myTCPSocket.close()
